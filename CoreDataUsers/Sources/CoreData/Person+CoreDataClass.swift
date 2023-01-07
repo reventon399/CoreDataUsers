1300ccf89c307362f709650373615c2d79d3b592
Alexey <alexeylosev1501@gmail.com>
2023-01-07 14:07:13 +0500

    add entity and subclasses.

---
+++ CoreDataUsers/Sources/CoreData/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataUsers
+//
+//  Created by Алексей Лосев on 07.01.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
